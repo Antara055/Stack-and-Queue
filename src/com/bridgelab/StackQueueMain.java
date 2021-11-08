64732e924c96e40a38af55c945bc808c63c69bc7
Antara055 <antara55.dgp10.1434@gmail.com>
2021-11-08 22:31:28 +0530

    Added: Stack and Queue

---
+++ src/com/bridgelab/StackQueueMain.java
@@ -0,0 +1,15 @@
+package com.bridgelab;
+
+public class StackQueueMain {
+    public static void main(String[] args){
+        System.out.printf("Welcome to Stack and Queue problem");
+    }
+}
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
