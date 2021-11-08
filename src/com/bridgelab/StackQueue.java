13834a130c93392fb1a6cb34bda4bcf1cb973b1f
Antara055 <antara55.dgp10.1434@gmail.com>
2021-11-08 22:59:34 +0530

    Added: Ability to Peak and Pop in stack

--- src/com/bridgelab/StackQueue.java
+++ src/com/bridgelab/StackQueue.java
@@ -3,16 +3,35 @@ package com.bridgelab;
 public class StackQueue {
     //Represent the Stack pointer top
     public static Node top = null;
-    public static void push(int data){
+
+    public static void push(int data) {
         Node node = new Node(data);
-        if (node == null){
+        if (node == null) {
             System.out.println("Stack is Under flow");
             node.next = null;
-        }else {
+        } else {
             node.next = top;
         }
         top = node;
         System.out.println("Pushed " + data);
+    }
+
+    //Method to display the stack Peek value
+    public void peek() {
+        if (top == null)
+            System.out.println("Stack is underflow");
+        else
+            System.out.println("Stack Peek value is : " + top.data);
+    }
+
+    //Method to display the stack Pop value
+    public void pop() {
+        if (top == null)
+            System.out.println("Stack is underflow");
+        else {
+            System.out.println("Stack Pooped value is : " + top.data);
+            top = top.next;
+        }
     }
 
     public void display() {
@@ -21,9 +40,11 @@ public class StackQueue {
         Node temp = new Node(top.data);
         temp = top;
         System.out.println("Stack Contains data in LIFO Order : ");
-        while (temp != null){
+        while (temp != null) {
             System.out.println(+temp.data);
             temp = temp.next;
         }
     }
 }
+
+
--- src/com/bridgelab/StackQueueMain.java
+++ src/com/bridgelab/StackQueueMain.java
@@ -2,13 +2,19 @@ package com.bridgelab;
 
 public class StackQueueMain {
     public static void main(String[] args){
-        System.out.printf("Welcome to Stack and Queue problem");
+        System.out.println("Welcome to Stack and Queue problem");
         StackQueue stack = new StackQueue();
         stack.push(70);
         stack.push(30);
         stack.push(56);
         //Displaying Stack
         stack.display();
+        while (stack.top != null){
+            stack.peek();
+            stack.pop();
+        }
+        if (stack.top == null)
+            System.out.println("Stack is empty");
     }
 }
 class Node{
